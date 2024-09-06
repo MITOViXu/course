b9d8da7fc1152323218ebb53f733f5e2ebc0c3fb
mtoan <109912739+MITOViXu@users.noreply.github.com>
2024-09-06 22:53:38 +0700

    feat(app): initial (dashboard) route

---
+++ course/app/layout.tsx
@@ -0,0 +1,25 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Course',
+  description: 'Created by mtoan',
+  icons: {
+    icon: '/next.svg', // Path to your favicon file
+  },
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
